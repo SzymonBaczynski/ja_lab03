b6edaf09516f4d21f31a855e496a57ff96318897
Junior Zoo Keeper <szymon.horzela@amister.pl>
2022-04-22 22:37:07 +0200

    Clear in Program.cs

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -58,43 +58,5 @@ class Program
     }
 }
 
-/*class Program
-{
-    [DllImport("DllAsm.dll")]
-    private static unsafe extern int ProcAsm2(int* a, int pos);
-
-    static int[] n1Array = { 1, 2, 3, 4, 5, 6 };
-    static void Main(string[] args)
-    {
-
-        unsafe
-        {
-            fixed (int* aAddress = &n1Array[0])
-            {
-                int c = ProcAsm2(aAddress, 5);
-                System.Console.WriteLine(c);
-            }
-        }
-
-    }
-}*/
-
-
-
-/*class Program
-{
-    [DllImport("DllAsm.dll")]
-    private static extern int ProcAsm1(int a, int b);
-    static void Main(string[] args)
-    {
-        int a = 10;
-        int b = 20;
-        unsafe
-        {
-            int c = ProcAsm1(a, b);
-            System.Console.WriteLine(c);
-        }
-    }
-}*/
 
 
